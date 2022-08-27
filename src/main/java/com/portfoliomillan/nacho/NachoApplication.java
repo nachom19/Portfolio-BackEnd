b52c4648b95e629d2aa2a4b2d5e7964374d4960e
Nacho <ignacio.millan1986@gmail.com>
2022-08-26 21:30:57 -0300

    Black end persona y experiencia a medias

---
+++ src/main/java/com/portfoliomillan/nacho/NachoApplication.java
@@ -0,0 +1,13 @@
+package com.portfoliomillan.nacho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NachoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NachoApplication.class, args);
+	}
+
+}
