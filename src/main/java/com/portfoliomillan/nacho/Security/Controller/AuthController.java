ed31b9a1fb54f2b6e0b0dc1aecfd7aa19925906f
Nacho <ignacio.millan1986@gmail.com>
2022-10-29 19:07:59 -0300

    Auth correcciones

--- src/main/java/com/portfoliomillan/nacho/Security/Controller/AuthController.java
+++ src/main/java/com/portfoliomillan/nacho/Security/Controller/AuthController.java
@@ -39,7 +39,7 @@ public class AuthController {
     @Autowired RolService rolService;
     @Autowired JwtProvider jwtProvider;
     
-    @PostMapping ("/nuevo_user")
+    @PostMapping ("/nuevo")
     public ResponseEntity<?>nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
         if (bindingResult.hasErrors())
           return new ResponseEntity(new Mensaje ("Campos completados erroneamente o email invalido"), HttpStatus.BAD_REQUEST);
